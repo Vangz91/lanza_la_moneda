8e9e6c57e235dc9e4781fb28b8223989f62ad9ac
Vangz91 <202562573+Vangz91@users.noreply.github.com>
2025-03-19 22:35:40 -0500

    Actualización para agregar número de lanzamientos, cálculos y gráfico

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 import streamlit as st
-import random
 
-# Título de la aplicación
 st.header('Lanzar una moneda')
 
-# Instrucción de la aplicación
-st.write('Haz clic en el botón para lanzar una moneda.')
+number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10)
+start_button = st.button('Ejecutar')
 
-# Botón para lanzar la moneda
-if st.button('Lanzar moneda'):
-    resultado = random.choice(['Cara', 'Cruz'])
-    st.write(f'El resultado es: {resultado}')
+if start_button:
+    st.write(f'Experimento con {number_of_trials} intentos en curso.')
+
+st.write('Esta aplicación aún no es funcional. En construcción.')
