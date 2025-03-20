542974638163bc3629fe8650bb2d35d09a8b88d5
Vangz91 <202562573+Vangz91@users.noreply.github.com>
2025-03-19 22:28:42 -0500

    Actualización de la aplicación para lanzar moneda

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 import streamlit as st
+import random
 
+# Título de la aplicación
 st.header('Lanzar una moneda')
 
-st.write('Esta aplicación aún no es funcional. En construcción.')
+# Instrucción de la aplicación
+st.write('Haz clic en el botón para lanzar una moneda.')
+
+# Botón para lanzar la moneda
+if st.button('Lanzar moneda'):
+    resultado = random.choice(['Cara', 'Cruz'])
+    st.write(f'El resultado es: {resultado}')
